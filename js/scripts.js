46f2405e875e2b32e4de0a25f4c1c85c0f63c31a
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-11-08 11:50:09 -0800

    add working showCost prototype function, erase traditional function. Add fully functioning cost calculator for size

--- js/scripts.js
+++ js/scripts.js
@@ -6,52 +6,63 @@ function Pizza (size, veggies){
   this.price = 0;
   }
 
+Pizza.prototype.costCalulator = function(size){
+  if (this.size === "undefined"){
+    price += 0;
+  };
 
-Pizza.prototype.costCalulator = function(size, veggies){
-    // if(this.veggies === "undefined"){
-    //   return 0;
-    // }
-    this.veggies.forEach(function(veggies){
-      this.price += 2;
-      console.log(this.price);
+  if (this.size === "12 Inch") {
+    this.price += 10;
+
+  }
+  if (this.size === "6 Inch") {
+    this.price += 8;
+  }
+
+  if (this.size === "2 Inch") {
+    this.price += 4;
+  }
+
+  if (this.veggies.length === [0]){
+    this.price += 2;
+  }
+  this.veggies.forEach(function(veggies){
+    this.price += 2;
+    console.log(this.veggies);
     });
-    if(this.size === "12 Inch"){
-      this.price += 6;
-    } else if (this.size === "6 Inch"){
-      this.price += 4;
-    } else if(this.price === "2 Inch"){
-      this.price += 2;
-    } else {
-      return this.price;
-    }
+
+    return this.price;
+};
+
+Pizza.prototype.showCost = function(price){
+  $("#show").show();
+  $("#price-span").html("$" + this.price);
 
 };
 
 
-function showCost(costCalculator) {
-    $("#show").show();
-    $("#price-span").html("$" + costCalculator);
-  };
+
+// function showCost() {
+//     $("#show").show();
+//     $("#price-span").html("$" + this.price);
+//   };
 
 
 $(document).ready(function(){
 $("#inputForm").submit(function(event){
   event.preventDefault();
 
-
       var sizeInput= $("#size").val();
-      console.log("pizza size" , sizeInput);
 
       var vegArray = [];
       $("input:checkbox[name=veggies]:checked").each(function(){
       var inputVeggies = $(this).val();
        vegArray.push(inputVeggies);
-
     });
 
-    let newPizza = new Pizza (sizeInput, vegArray)
-    newPizza.costCalulator();
-
+    let newPizza = new Pizza (sizeInput, vegArray);
+    newPizza.costCalulator(sizeInput, vegArray);
+    newPizza.showCost();
 
 
 });
