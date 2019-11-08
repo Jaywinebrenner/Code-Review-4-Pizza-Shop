e229b045ccde48f865bfa5a3013e1c1a768ec230
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-11-08 14:03:55 -0800

    add sauce if/else. wrestle with sauce bug

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,52 @@
 
 function Pizza (size, sauce, veggies){
   this.size = size;
+  this.sauce = sauce;
   this.veggies= veggies;
   this.price = 0;
   }
 
-Pizza.prototype.costCalulator = function(size){
+Pizza.prototype.costCalulator = function(size, sauce, veggies){
   if (this.size === "undefined"){
     price += 0;
   }
-
   if (this.size === "12 Inch") {
     this.price += 10;
-
   }
   if (this.size === "6 Inch") {
     this.price += 8;
   }
-
   if (this.size === "2 Inch") {
     this.price += 4;
   }
+console.log(this.sauce);
+  if (this.sauce == "Tomato") {
+    this.price += 3;
+    console.log(this.price);
+  }
+  if (this.sauce == "Liquid Larynx" || "Bat Blood") {
+    this.price += 5;
+    console.log(this.price);
+  }
+  if(this.sauce === "Extra Spicy") {
+    this.price += 8;
+    console.log(this.price);
 
-
-
-  this.veggies.forEach(function(veggies){
-    this.price += 2;
-    });
+  }
+  //
+  // if (this.size === "2 Inch") {
+  //   this.price += 4;
+  // }
+  // this.veggies.forEach(function(veggies){
+  //   this.price += 2;
+  //   });
 
     return this.price;
 };
 
 Pizza.prototype.showCost = function(price){
   $("#show").show();
-  $("#price-span").html("$" + this.price);
+  $("#price-p").html("$" + this.price);
 
 };
 
