9094daba6cc07bd7a6ea30d49ae5a1b39cb2b2fb
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-11-08 08:50:31 -0800

    extract value from inputs of size.

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,28 @@ function Pizza (size, veggies){
 
 
 
-
+let newPizza = new Pizza(sizeInput, vegArray)
 
 $(document).ready(function(){
 $("#inputForm").submit(function(event){
   event.preventDefault();
 
 
+      var sizeInput= $("#size").val();
+      console.log(size);
 
+      var veggiesInputs = $("#veggies:checked");
+      console.log(veggiesInputs);
+      var vegArray = [];
+      var checkboxArr=[];
+veggiesInputs.forEach(function(veg){
+    vegArray.push(veg.val());
+});
+
+console.log(vegArray);
+
+// for(i=0; i <= veggiesInputs.length; i++){
+//   vegArray.push(veggiesInputs[i]);
 
 
 
