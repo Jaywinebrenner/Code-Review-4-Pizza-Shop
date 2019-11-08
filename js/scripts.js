502afd650baaf967d418f5bb5dcffe53f8f4c71d
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-11-08 10:04:47 -0800

    add css stylings, import google fonts

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,23 @@ function Pizza (size, veggies){
   this.price = 0;
   }
 
-Pizza.prototype.costCalulator = function(){
+Pizza.prototype.costCalulator = function(size, veggies){
+    if(this.veggies === "undefined"){
+      return 0;
+    }
+    this.veggies.forEach(function(){
+      this.price += 2
+    });
+    if(this.size === "12 Inch"){
+      this.price += 6;
+    } else if (this.size === "6 Inch"){
+      this.price += 4;
+    } else if(this.price += "2 Inch"){
+      this.price += 2;
+    } else {
+      return this.price;
+    }
+
 
 };
 
